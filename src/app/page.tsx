66e2bd023163bd53ab8af11726008c092dd89a29
teaser <developer@teasersoftware.com>
2025-11-03 17:33:22 +0700

    Init base repo

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (
+    <div className="font-spectral grid grid-cols-12 grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen bg-primary overflow-hidden pt-40">
+    </div>
+  );
+}
